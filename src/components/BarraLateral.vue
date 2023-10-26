e879990db80d6fb983c54478e61c252c9a02d072
Robson Wendel <105562688+robsu17@users.noreply.github.com>
2023-10-26 11:57:55 -0300

    create project

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <h1>
+            <img src="../assets/alura.png" alt="">
+        </h1>
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';    
+
+export default defineComponent({
+    name: 'BarraLateral'
+})
+</script>
+
+<style scoped>
+header {
+    background: #0d3b66;
+    padding: 1rem;
+    widows: 100%;
+    height: 100vh;
+}
+@media only screen and (max-width: 768px) {
+    header {
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+</style>
